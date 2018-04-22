8e2c30329a796a51c4a8988a3320a33ff050457b
Cálcio <calcionit@gmail.com>
2018-04-22 13:58:14 -0300

    Add real content in _info.php

--- frontend/views/site/_info.php
+++ frontend/views/site/_info.php
@@ -6,10 +6,23 @@ use yii\helpers\Html;
                 <div class="block-heading-one">
                     <h3><i class="fa fa-angle-double-right color"></i> Informações sobre a acupuntura</h3>
                 </div>
+
+                <p>
+                    Sua origem ocorreu na China há aproximadamente 4.500 anos. No entanto, apesar de sua antiguidade, esta ciência
+                    milenar continua evoluindo e mostrando sua eficácia.<br><br>
+                </p>
+
+                <p>
+                    Acupuntura é uma técnica, dentro da medicina chinesa, que harmoniza o corpo através da inserção de agulhas específicas
+                    nos canais energéticos, possibilitando assim a prevenção e cura das diversas patologias físicas e emocionais.<br><br>
+                </p>
                 
-                <p>Maecenas etos sit amet, consectetur adipiscing elit. Terminal volutpat rutrum metro amet sollicitudin interdum. Suspendisse pulvinar, velit etos pharetra interdum, ante tellus gravida mollis tellus neque vitae elit. Mauris adipiscing mauris fringilla turpis interdum pulvinar nisi malesuada, consectetur adipiscing libero est cuprum.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut volutpat rutrum eros sit amet sollicitudin. Suspendisse pulvinar, velit nec pharetra interdum, ante tellus ornare mi, et mollis tellus neque vitae elit. Mauris adipiscing mauris.</p>
-                <p>Maecenas etos sit amet, consectetur adipiscing elit. Terminal volutpat rutrum metro amet sollicitudin interdum. Suspendisse pulvinar, velit etos pharetra interdum, elit. Mauris mauris  turpis interdum pulvinar nisi malesuada, consectetur adipiscing libero est cuprum.</p>
+                <p>
+                    O tratamento da Acupuntura está baseado na teoria do Yin e Yang, no conceito dos 5 movimentos (Fogo, Terra, Metal, Água e Madeira),
+                    na Energia do corpo (QI) e no sangue (XUE). Após o diagnóstico energético dos padrões de desarmonia do corpo,
+                    as agulhas são inseridas nos pontos de acupuntura, visando restabelecer a circulação energética, com isso,
+                    levar o corpo a uma harmonia.
+                </p>
                 <br />
             </div>
             <div class="col-md-6 col-sm-6">
@@ -17,8 +30,8 @@ use yii\helpers\Html;
                     <h3>&nbsp;</h3>
                 </div>
 
-                <?= Html::img('@web/img/gallery/big/acupuntura_001.jpg', 
-                    ['alt' => 'acupuntura', 'class' => 'img-responsive']) ?>                
+                <?= Html::img('@web/img/gallery/big/acupuntura_001.jpg',
+                    ['alt' => 'acupuntura', 'class' => 'img-responsive']) ?>
                 <br />
             </div>
         </div>
